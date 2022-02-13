7ce45f7ac6f43aa3d06cc464fbd0a4681e34a3f6
realme1 <realmeapp9@gmail.com>
2022-02-13 22:36:36 +0530

    firm name set as FD in the name of food delivery

--- src/main/java/com/yts/restarent1/Restarent1Controller.java
+++ src/main/java/com/yts/restarent1/Restarent1Controller.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/zomoto")
+@RequestMapping("/fd")
 public class Restarent1Controller {
 
     @GetMapping("")
     String welcomeMsz(){
-        return "welcome to zomato services";
+        return "welcome to fd services";
     }
 }

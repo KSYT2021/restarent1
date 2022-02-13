a38d109a1e20b248a823a0497a3f6eef0ca58c3c
realme1 <realmeapp9@gmail.com>
2022-02-13 22:12:15 +0530

    basic initial code added

---
+++ src/main/java/com/yts/restarent1/Restarent1Application.java
@@ -0,0 +1,13 @@
+package com.yts.restarent1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Restarent1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Restarent1Application.class, args);
+    }
+
+}
